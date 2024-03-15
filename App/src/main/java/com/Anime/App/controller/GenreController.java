6bb16f041856e7c95618a0e8fb50d4ab39c90c99
vallarasuSJ <vallarasu.sakthivel@kumaran.com>
2024-03-15 17:07:45 +0530

    first commit

--- App/src/main/java/com/Anime/App/controller/GenreController.java
+++ App/src/main/java/com/Anime/App/controller/GenreController.java
@@ -4,11 +4,9 @@ import com.Anime.App.model.Genre;
 import com.Anime.App.request.GenreRequest;
 import com.Anime.App.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
